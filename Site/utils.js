15ca51d42b0c06eceffa54931d38234b677b179f
Nmerryman <nmerryman9001@gmail.com>
2023-04-23 23:01:59 -0500

    Made a change that should allow me to run inserted script tags

--- Site/utils.js
+++ Site/utils.js
@@ -1,16 +1,20 @@
 
 function fetcher(call_name, target_id, argument=[]) {
     var xhttp = new XMLHttpRequest;
-    arg_mod = ""
+    var arg_mod = ""
     if (argument.length != 0) {
         for (a = 0; a < argument.length; a++){
             arg_mod = arg_mod + "&arg" + a + "=" + argument[a];
         }
     }
     xhttp.open("GET", "executor.php?" + call_name + "=true" + arg_mod);
-    document.getElementById(target_id).innerText = "Working";
+    var target = document.getElementById(target_id);
+    target.innerText = "Working";
     xhttp.onload = function() {
-        document.getElementById(target_id).innerHTML = xhttp.responseText;
+        target.innerText = "";
+        var part = document.createElement("div");
+        part.innerHTML = xhttp.responseText;
+        target.appendChild(part);
     }
     xhttp.send();
     console.log("a:" + arg_mod);
